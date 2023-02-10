d4dcd65fd7e52d352066da5c31fc3ccba7e4e324
NicHanks <han22057@byui.edu>
2023-02-09 19:29:59 -0700

    update

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Word()
+{
+    
+    private string _wordShown = "";
+    private string _wordHide = "";
+
+
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+
+    }
+    public void IsHidden()
+    {
+
+    }
+    public void GetRenderedText()
+    {
+
+    }
+}
